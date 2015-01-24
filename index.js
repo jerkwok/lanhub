119803dfa0851a8dda74b46a5c4fe0350a9bb86f
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-01-24 15:22:16 -0500

    created basic chat interface

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+var express = require('express')();
 var app = require('express')();
 var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+//static connection stuff
+var connect = require('connect');
+var serveStatic = require('serve-static');
+app.use(serveStatic('public'));
 
 app.get('/',function(req,res){
-	res.send(__dirname + '/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, function(){
