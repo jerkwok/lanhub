e84cbbe4666e686d00ff0e0e8717ec537d1466a9
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-01-25 00:16:18 -0500

    removed unnecessary js files

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3000, function(){
-	console.log('listening on port 3000');
+http.listen(3001, function(){
+	console.log('listening on port 3001');
 });
