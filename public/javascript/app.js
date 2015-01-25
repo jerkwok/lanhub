a4eb6bd71c8eb6af26786690be3fd436b4a80d12
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-01-25 17:39:35 -0500

    UI tweaks

---
+++ public/javascript/app.js
@@ -0,0 +1,25 @@
+var socket = io();
+	$('form').submit(function(){
+	    socket.emit('chat message', $('#m').val());
+	    $('#m').val('');
+    return false;
+  	});
+  	
+	socket.on('chat message', function(msg){
+	    switch(msg){
+	    	case ":kyubey":
+		    	$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/kyubey.png">'));
+		    	break;
+		    case ":snout":	    
+	    		$('#messages').append($('<li><iframe width="560" height="315" src="//www.youtube.com/embed/-Lmh4vT-bSE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'));
+	    		break;
+	    	default: 
+	    		$('#messages').append($('<li>').text(msg));
+		}
+	scrollToBottom('messagebox');
+	});
+
+	socket.on('server message', function(msg){
+		$('#messages').append($('<li style="color:#F00">').text(msg));
+		scrollToBottom('messagebox');
+});
