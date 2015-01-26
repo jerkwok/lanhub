4979a51360a21f9a34866510399ccc9063a260b1
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-01-25 19:38:29 -0500

    ui fixes: removed ability to send whitespace, and fixed chat log alignment

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,30 +6,32 @@ var socket = io();
   	});
 
 	socket.on('chat message', function(msg){
-	    switch(msg){
-	    	case ":kyubey":
-		    	$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/kyubey.png">'));
-		    	break;
-		    case ":deko":	    
-	    		$('#messages').append($('<li><iframe width="560" height="315" src="//www.youtube.com/embed/-Lmh4vT-bSE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'));
-	    		break;
-		    case ":snout":	    
-	    		$('#messages').append($('<li><iframe width="560" height="315" src="//www.youtube.com/embed/-Lmh4vT-bSE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'));
-	    		break;
-	    	case ":sugoi":
-				$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/sugoi.gif">'));
-	    		break;
-	    	case ":baka":
-				$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/baka.gif">'));
-	    		break;
-	    	case ":gasp":
-				$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/gasp.png">'));
-	    		break;
-	    	case "":
-				//$('#messages').append($(''));
-	    		break;
-	    	default: 
-	    		$('#messages').append($('<li>').text(msg));
+		if(msg.trim().length != 0){
+		    switch(msg){
+		    	case ":kyubey":
+			    	$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/kyubey.png">'));
+			    	break;
+			    case ":deko":	    
+		    		$('#messages').append($('<li><iframe width="560" height="315" src="//www.youtube.com/embed/-Lmh4vT-bSE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'));
+		    		break;
+			    case ":snout":	    
+		    		$('#messages').append($('<li><iframe width="560" height="315" src="//www.youtube.com/embed/-Lmh4vT-bSE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'));
+		    		break;
+		    	case ":sugoi":
+					$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/sugoi.gif">'));
+		    		break;
+		    	case ":baka":
+					$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/baka.gif">'));
+		    		break;
+		    	case ":gasp":
+					$('#messages').append($('<li><img src="http://www.lyninx.com/lanhub/gasp.png">'));
+		    		break;
+		    	case "":
+					//$('#messages').append($(''));
+		    		break;
+		    	default: 
+		    		$('#messages').append($('<li>').text(msg));
+			}
 		}
 	scrollToBottom('messagebox');
 	});
