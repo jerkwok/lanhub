5167a71839dbb897d23885ed9d4c33d65d634172
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-01-26 09:30:19 -0500

    temp username fix

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg,username);
+    io.emit('chat message', msg,"username");
     //scrollToBottom('messagebox');
   });
 });
